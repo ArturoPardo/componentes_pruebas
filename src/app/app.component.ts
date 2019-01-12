1b103f86ded3949655901f573d368407264d7ec9
Arturo Pardo <arturopardosanchez@gmail.com>
2019-01-12 12:17:38 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,26 @@ export class AppComponent {
   text = '';
   showDiv: boolean = false;
   cambioCheck: boolean = false;
+
+ 
+
+mios: Array<{ nombre: string; apellidos:string;}> =[
+  { nombre: 'Arturo', apellidos: '😀' },
+  { nombre: 'Antonia', apellidos: '😂' },
+
+];
+emojis: Array<{ text: string; icon: string }> = [
+  { text: 'normal', icon: '😀' },
+  { text: 'risas', icon: '😂' },
+  { text: 'lol', icon: '🤣' },
+  { text: 'smile', icon: '😃' },
+  { text: 'putada', icon: '😅' },
+  { text: 'blink', icon: '😉' },
+  { text: 'happy', icon: '😊' },
+];
+
+
+
   escribirText(ev) {
     this.text = ev.target.value;
   }
